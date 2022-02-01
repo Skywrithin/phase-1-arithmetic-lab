c21052cf1a914030ed15f732aac65c394a2f5bd9
Kaleb <kaleb.franken7@gmail.com>
2022-01-31 18:42:10 -0700

    commit tree 8786946128c38fef9e5f4275a75203288409159c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.min(1, 2, 3, 4, 5);
+
+let num3 =54;
+let num4 =50;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
